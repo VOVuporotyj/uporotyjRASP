ea1f0c0328883fa854eff54c2d6bc4d32634379a
VOVuporotyj <Vladimir59912@gmail.com>
2017-03-09 10:22:44 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+﻿package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	resp := r.PostFormValue("answer")
+	fmt.Print(resp)
+	t, err := template.ParseFiles("C:\\Work\\testWork\\templates\\index.html", "C:\\Work\\testWork\\templates\\header.html") //"C:\\Work\\testWork\\templates\\footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+
+}
+
+//test
+func main() {
+	fmt.Println("listening on port :3000")
+
+	http.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(":3000", nil)
+
+}
